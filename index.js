35f83d6f6de9512a03b8780ad152190991e0d25b
sinbarxie <sinbarxie@tencent.com>
2020-08-05 18:53:07 +0800

    2

--- index.js
+++ index.js
@@ -14,13 +14,86 @@ import 'tinymce/themes/silver';
 // Any plugins you want to use has to be imported
 import 'tinymce/plugins/paste';
 import 'tinymce/plugins/link';
+// // Any plugins you want to use has to be imported
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/print';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/importcss';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/visualchars';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/template';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/hr';
+import 'tinymce/plugins/pagebreak';
+import 'tinymce/plugins/nonbreaking';
+import 'tinymce/plugins/anchor';
+import 'tinymce/plugins/toc';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/textpattern';
+import 'tinymce/plugins/noneditable';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/quickbars';
+import 'tinymce/plugins/emoticons';
 
+const s = require('!!raw-loader!./lang/zh_CN')
+function codeToBlob (code) {
+    let blob = new Blob([code], {type: 'text/javascript'}); // 生成js文件对象
+    let objectURL = window.URL.createObjectURL(blob); // 生成js文件的url
+    return objectURL;
+}
+const url = codeToBlob(s)
 // Initialize the app
 tinymce.init({
     selector: '#temp',
-    plugins: ['paste', 'link'],
-    content_css:'default'
+    plugins: 'print preview paste importcss searchreplace directionality code visualblocks visualchars fullscreen image link media codesample table charmap hr pagebreak nonbreaking toc insertdatetime advlist lists wordcount imagetools textpattern noneditable charmap',
+    menubar: 'file edit view insert format tools table help',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap | fullscreen  preview save print | insertfile image media link | ltr rtl',
+    toolbar_sticky: true,
+    image_advtab: true,
+    file_picker_callback: function (callback, value, meta) {
+        /* Provide file and text for the link dialog */
+        if (meta.filetype === 'file') {
+            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+        }
+
+        /* Provide image and alt text for the image dialog */
+        if (meta.filetype === 'image') {
+            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+        }
+
+        /* Provide alternative source and posted for the media dialog */
+        if (meta.filetype === 'media') {
+            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+        }
+    },
+    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+    height: 600,
+    image_caption: true,
+    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+    noneditable_noneditable_class: "mceNonEditable",
+    toolbar_mode: 'sliding',
+    contextmenu: 'link image imagetools table',
+    skin: false,
+    content_css: false,
+    content_style: '',
+    language_url: url,
+    language: 'zh_CN'
 });
 
-tinymce.init({ selector: '#temp' });
 
